6f76d7106248a2aaa7493ef3cd86db93d37980d5
Ben Cookman <benlcookman@gmail.com>
2022-10-11 11:03:49 +0100

    errors on forward euler
    fonts in plots are being weird
    
    remove module wrapping

--- Ben Code/DE_Solver1.jl
+++ Ben Code/DE_Solver1.jl
@@ -1,12 +1,8 @@
-module DE_Solver
-
-using Dates, Plots, BenchmarkTools, Statistics, LinearAlgebra
-
-export run_advection_forward_euler, Butcher1_forward_euler
+using Dates, Plots, BenchmarkTools, Statistics, LinearAlgebra, LaTeXStrings, GR
 
 function error_calculate(data_correct, data_simulated, p)
-    err_data = data_correct - data_simulated
-    err_norm = norm(err_data, p) # Use l2 norm in this case
+    err_data = abs.(data_correct - data_simulated)
+    err_norm = norm(err_data, p)
     err_max  = maximum(err_data)
     return Dict(:data => err_data, :norm => err_norm, :max => err_max)
 end
@@ -76,11 +72,11 @@ function run_advection_forward_euler()
     # @benchmark advection_forward_euler($(test_x_start), $(test_x_end), $(test_t_end), $(test_v), false, false)
 end
 
-function Butcher1_forward_euler(t_end, t_res, u_init, save_plot, save_type, return_error)
-    t_array = range(0.0, t_end, t_res) |> collect
+function butcher1_forward_euler(t_end, t_res, u_init; save_plot=true, save_type="png")
+    t_array = range(0.0, t_end, t_res+1) |> collect
     Δt = t_end/t_res
     f(t, u) = (u-2t*u^2)/(1+t)
-    u_correct(t) = (1+t)/(t+1/u_init)
+    u_correct(t) = (1+t)/(t^2+1/u_init)
 
     u = u_init
     u_sims = [u]
@@ -91,9 +87,17 @@ function Butcher1_forward_euler(t_end, t_res, u_init, save_plot, save_type, retu
     end
 
     if save_plot
-        new_plot = plot(t_array, u_sims)
-        plot!(new_plot, t_array, u_correct.(t_array),
-              color=:red, ylims=(0.2, 0.8))
+        new_plot = plot(
+            t_array, u_sims,
+            label="h=$(Δt)", marker=".",
+            xticks=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0], yticks=[0.2, 0.4, 0.6, 0.8]
+        )
+        t_plot = range(0.0, t_end, 1000) |> collect
+        plot!(
+            new_plot, t_plot, u_correct.(t_plot),
+            color=:red, ylims=(0.2, 0.8), label="u_correct"
+        )
+        display(new_plot)
 
         dtstring = Dates.format(now(), "DY-m-d-TH-M-S")
         fname = "Ben Code/output/plot-$dtstring.$(save_type)" # NEED TO IMPROVE
@@ -101,8 +105,26 @@ function Butcher1_forward_euler(t_end, t_res, u_init, save_plot, save_type, retu
     end
 
     # Calculate error on whole series
-    return_error && error_calculate(u_correct.(t_array), u_sims, 2)
+    error_calculate(u_correct.(t_array), u_sims, 2)
 end
 
+function run_butcher1()
+    t_end = 1.0
+    u_init = 0.4
+    t_res_array = [5*2^i for i in 0:20]
+    err_plot_array = []
 
+    for t_res in t_res_array
+        err = butcher1_forward_euler(t_end, t_res, u_init; save_plot=false)
+        # println("res = $(t_res)")
+        # println("error norm = $(err[:norm])")
+        # println("error at end = $(err[:data][end])")
+        push!(err_plot_array, err[:norm])
+    end
+
+    plt = plot(
+        t_res_array, err_plot_array,
+        xscale=:log10, yscale=:log10, xlabel=L"t_{res}", ylabel=L"l^2" * " error",
+        title="test title 1"
+    )
 end
