27531b41710ea53366882a811827ad579712e0c2
Ben Cookman <benlcookman@gmail.com>
2022-11-10 11:10:27 +0000

    integration matrix change
    move from poly integrating over dt's to integrating analytically

--- Ben Code/IDC_test1.jl
+++ Ben Code/IDC_test1.jl
@@ -22,7 +22,7 @@ function IDC(f, a, b, α, N, p)
     η = zeros(N + 1)
     η[1] = α
 
-    S = compute_integration_matrix(M; integral_resolution=1000)
+    S = integration_matrix_equispaced(M)
 
     for j in 0:(J-1)
         # Prediction loop
@@ -56,7 +56,7 @@ function IDC_single(f, a, b, α, N, p)
     η = zeros(N+1)
     η[1] = α
 
-    S = compute_integration_matrix(N; integral_resolution=100)
+    S = integration_matrix_equispaced(N)
 
     # Prediction loop
     for m in 1:N
@@ -71,149 +71,6 @@ function IDC_single(f, a, b, α, N, p)
     end
 
     return η
-end
-
-""" Integral deferred correction with a single group - use matrix """
-function IDC_single_matrix(f, a, b, α, N, p)
-    # Initialise variables
-    t = range(a, b, N+1) |> collect
-    Δt = (b - a)/N
-    η = Array{Float64, 2}(undef, p, N+1)
-    η[:, 1] .= α
-
-    println(Δt)
-    S = compute_integration_matrix(N; integral_resolution=100)
-
-    # Prediction loop
-    for m in 1:N
-        η[1, m + 1] = η[1, m] + Δt*f(t[m], η[1, m])
-    end
-    # Correction loop
-    for l in 2:p
-        η[l, 1] = η[l-1, 1]
-        for m in 1:N
-            η[l, m + 1] = η[l, m] + Δt*(f(t[m], η[l, m]) - f(t[m], η[l-1, m])) + Δt*sum(S[m, i]*f(t[i], η[l-1, i]) for i in 1:(N+1))
-        end
-    end
-
-    return η[end, :]
-end
-
-""" Integral deferred correction with a single group - use error and residuals """
-function IDC_single_error(f, a, b, α, N, p)
-    # Initialise variables
-    t = range(a, b, N+1) |> collect
-    Δt = (b - a)/N
-    η = Array{Float64, 1}(undef, N+1)
-    η[1] = α
-
-    S = compute_integration_matrix(N; integral_resolution=1000)
-
-    # Prediction loop
-    for m in 1:N
-        η[m + 1] = η[m] + Δt*f(t[m], η[m])
-    end
-    # Correction loop
-    for l in 2:p
-        δ = Array{Float64, 1}(undef, N + 1)
-        δ[1] = 0
-        for m in 1:N
-            Δϵ = Δt*sum(S[m, i]*f(t[i], η[i]) for i in 1:(N + 1)) - η[m + 1] + η[m] # difference in residuals at m+1 and m
-            δ[m + 1] = δ[m] + Δt*(f(t[m], η[m] + δ[m]) - f(t[m], η[m])) + Δϵ
-            # l == 3 && println(Δt*(f(t[m], η[m] + δ[m]) - f(t[m], η[m])) + Δϵ)
-        end
-        η .+= δ
-    end
-
-    return η
-end
-
-""" Integral deferred correction with a single group - use lagrange polynomial"""
-function IDC_single_poly(f, a, b, α, N, p)
-    # Initialise variables
-    t = range(a, b, N+1) |> collect
-    Δt = (b - a)/N
-    η = Array{Float64, 1}(undef, N+1)
-    η[1] = α
-
-    # Prediction loop
-    for m in 1:N
-        η[m + 1] = η[m] + Δt*f(t[m], η[m])
-    end
-    # Correction loop
-    for l in 2:p
-        δ = Array{Float64, 1}(undef, N + 1)
-        δ[1] = 0
-
-        f_poly(x) = sum(f(t[i], η[i])*prod((x - t[k])/(t[i] - t[k]) for k in 1:(N + 1) if k != i) for i in 1:(N + 1))
-
-        t_int = range(a, b, 1000) |> collect
-        dt = t_int[2] - t_int[1]
-        integrals = [sum(f_poly(t_val) for t_val in t_int if t_val <= t[m]).*dt for m in 1:(N + 1)]
-        ϵ = integrals .+ α .- η
-
-        for m in 1:N
-            δ[m + 1] = δ[m] + Δt*(f(t[m], η[m] + δ[m]) - f(t[m], η[m])) + ϵ[m + 1] - ϵ[m]
-        end
-        η .+= δ
-    end
-
-    return η
-end
-
-""" Integral deferred correction with a single group - use lagrange polynomial and Newton-Cotes weights """
-function IDC_single_poly_NC(f, a, b, α, N, p)
-    # Initialise variables
-    t = range(a, b, N+1) |> collect
-    Δt = (b - a)/N
-    η = Array{Float64, 1}(undef, N+1)
-    η[1] = α
-
-    # Prediction loop
-    for m in 1:N
-        η[m + 1] = η[m] + Δt*f(t[m], η[m])
-    end
-    # Correction loop
-    for l in 2:p
-        δ = Array{Float64, 1}(undef, N + 1)
-        δ[1] = 0
-
-        f_poly(x) = sum(f(t[i], η[i])*prod((x - t[k])/(t[i] - t[k]) for k in 1:(N + 1) if k != i) for i in 1:(N + 1))
-        integrals = newton_cotes_integration(t, N, f_poly)
-        ϵ = integrals .+ α .- η
-
-        for m in 1:N
-            δ[m + 1] = δ[m] + Δt*(f(t[m], η[m] + δ[m]) - f(t[m], η[m])) + ϵ[m + 1] - ϵ[m]
-        end
-        η .+= δ
-    end
-
-    return η
-end
-
-# Function to calculate closed Newton-Cotes Quadrature weights - FROM BRADLEYS CODE
-function newton_cotes_weights(t, n)
-    weights = zeros(n+1)
-    for j in 1:n+1
-        u = union(1:j-1, j+1:n+1)
-        coeff = [1, -t[u[1]]] / (t[j] - t[u[1]])
-        for l in 2:n
-            coeff = ([coeff; 0] - t[u[l]]*[0; coeff]) / (t[j] - t[u[l]])
-        end
-        evalb = sum((coeff ./ collect(n+1:-1:1)) .* t[end] .^ collect(n+1:-1:1))
-        evala = sum((coeff ./ collect(n+1:-1:1)) .* t[1] .^ collect(n+1:-1:1))
-        weights[j] = evalb - evala
-    end
-    return(weights)
-end
-# Function to approximately integrate a polynomial interpolation of f(u, t) using Newton-Cotes
-function newton_cotes_integration(t, n, f_pol)
-    int_hat = zeros(length(t))
-    for j in 2:length(t)
-        sub_t = [i*(t[j]-t[1])/n + t[1] for i in 0:n]
-        int_hat[j] = sum(f_pol.(sub_t) .* newton_cotes_weights(sub_t, n))
-    end
-    return(int_hat)
 end
 
 function IDC_test_func(f, y, α, t_end, p, N_array)
@@ -304,28 +161,27 @@ end
 
 function integration_matrix_test()
     # Set up test
-    integral_resolution = 1000
     # t_end = 1.0
     # f(t) = cos(t)
     # integral_exact = sin(t_end)
-    t_end = 10*pi
-    f(t) = cos(t)^2
-    integral_exact = t_end/2 + sin(2*t_end)/4
-    # t_end = 100
-    # f(t) = sqrt(t)
-    # integral_exact = 2*(t_end)^(1.5)/3
+    # t_end = 1
+    # f(t) = cos(t)^2
+    # integral_exact = t_end/2 + sin(2*t_end)/4
+    t_end = 1
+    f(t) = sqrt(t)
+    integral_exact = 2*(t_end)^(1.5)/3
     # t_end = 0.001
     # f(t) = cos(t)*exp(sin(t))
     # integral_exact = exp(sin(t_end)) - 1
 
     # Do test
     integral_approximations = Array{Float64, 1}(undef, 0)
-    sum_resolutions = 1:2:100
+    sum_resolutions = 2:2:30
     for sum_resolution in sum_resolutions
-        t_sample = range(0, t_end, sum_resolution+1) |> collect
+        t_sample = 0:(sum_resolution + 1) |> collect
         f_sample = f.(t_sample)
-        S = compute_integration_matrix(sum_resolution; integral_resolution=integral_resolution)
-        integral_approx = t_end * sum(sum(S[:, i] .* f_sample[i] for i in 1:(sum_resolution+1))) / sum_resolution
+        S = integration_matrix_equispaced(sum_resolution)
+        integral_approx = sum(S[1, i] .* f_sample[i] for i in 1:(sum_resolution+1))
 
         println(integral_approx)
         push!(integral_approximations, integral_approx)
@@ -340,6 +196,6 @@ function integration_matrix_test()
         size=(1200, 900), thickness_scaling=1.5
     )
     dtstring = Dates.format(now(), "DY-m-d-TH-M-S")
-    fname = "Ben Code/output/tests/int-matrix-err-cos-2-10-$dtstring.png"
+    fname = "Ben Code/output/tests/int-matrix-new-err-cos-2-10-$dtstring.png"
     savefig(test_plot, fname)
 end
--- Ben Code/ProjectTools.jl
+++ Ben Code/ProjectTools.jl
@@ -1,28 +1,35 @@
 module ProjectTools
 
-using LinearAlgebra, Statistics
+using LinearAlgebra, Statistics, PyCall
 
 export
     err_abs,
     err_cum,
     err_norm,
     err_max,
-    compute_integration_matrix
+    integration_matrix_equispaced
 
 err_abs(exact, approx) = abs.(exact - approx)
 err_cum(exact, approx) = [sum(err_abs(exact[1:i], approx[1:i])) for i in 1:length(data_correct)]
 err_norm(exact, approx, p) = norm(err_abs(exact, approx), p)
 err_max(exact, approx)  = maximum(err_abs(exact, approx))
 
-function compute_integration_matrix(M; integral_resolution=10)
-    [compute_integration_matrix_entry(M, m, i, integral_resolution)
-     for m = 0:(M-1), i = 0:M]
-end
+function integration_matrix_equispaced(M)
+    scipy_interpolate = pyimport("scipy.interpolate")
 
-function compute_integration_matrix_entry(M, m, i, integral_resolution)
-    t_array = range(m, m+1, integral_resolution) |> collect
-    dt = t_array[2] - t_array[1]
-    sum(prod((t-k)/(i-k) for k = 0:M if k != i)*dt for t in t_array[1:end-1])
+    x = collect(Float64, 0:M)
+    # Calculate anti-derivatives of polynomial interpolants to δᵢⱼ
+    q_func = []
+    for i in 1:(M + 1)
+        yᵢ = zeros(Float64, M + 1)
+        yᵢ[i] = 1.0
+        pᵢ_coeffs = scipy_interpolate.lagrange(x, yᵢ).c |> reverse  # Lagrange interpolant to yᵢ at x
+        qᵢ_coeffs = pᵢ_coeffs ./ collect(Float64, 1:length(pᵢ_coeffs))              # Anti-derivative of pᵢ
+        qᵢ_func(t) = sum(qᵢⱼ*t^j for (j, qᵢⱼ) in enumerate(qᵢ_coeffs))
+        push!(q_func, qᵢ_func)
+    end
+    # Use anti-derivatives to evaluate integration matrix
+    return [qᵢ_func(m + 1) - qᵢ_func(m) for m in 0:(M - 1), (_, qᵢ_func) in enumerate(q_func)]
 end
 
 end
